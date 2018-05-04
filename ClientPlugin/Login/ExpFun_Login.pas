8bf6c799bcb14f0c509407ac8698dccce1ae3e53
59079096 <38979394+59079096@users.noreply.github.com>
2018-05-04 21:50:31 +0800

    Add files via upload

---
+++ ClientPlugin/Login/ExpFun_Login.pas
@@ -0,0 +1,84 @@
+unit ExpFun_Login;
+
+{******************************************************************************}
+{                                                                              }
+{                    住院医生站插件功能实现单元 hc 2016-6-7                    }
+{                                                                              }
+{                 导出函数单元，为主程序和插件交互提供以下函数：               }
+{                      1.GetPluginInfo获取插件信息                             }
+{                      2.ExecFunction调用插件某功能                            }
+{******************************************************************************}
+
+interface
+
+uses
+  PluginIntf, FunctionIntf;
+
+/// <summary>
+/// 返回插件信息和注册插件提供的功能
+/// </summary>
+/// <param name="AIPlugin">插件信息</param>
+procedure GetPluginInfo(const AIPlugin: IPlugin); stdcall;
+
+/// <summary>
+/// 卸载插件
+/// </summary>
+/// <param name="AIPlugin">插件信息</param>
+procedure UnLoadPlugin(const AIPlugin: IPlugin); stdcall;
+
+/// <summary>
+/// 执行功能
+/// </summary>
+/// <param name="AIService">功能类型</param>
+procedure ExecFunction(const AIFun: ICustomFunction); stdcall;
+
+exports
+   GetPluginInfo,
+   ExecFunction,
+   UnLoadPlugin;
+
+implementation
+
+uses
+  FunctionConst, FunctionImp, PlugInConst, frm_login, Vcl.Forms;
+
+// 插件信息和注册功能
+procedure GetPluginInfo(const AIPlugin: IPlugin); stdcall;
+begin
+  AIPlugin.Author := 'HC';  // 插件模块作者
+  AIPlugin.Comment := '完成系统登录';  // 插件说明
+  AIPlugin.ID := PLUGIN_LOGIN; // 插件GUID，即唯一标识
+  AIPlugin.Name := '系统登录';  // 插件功能或业务名称
+  AIPlugin.Version := '1.0.0';  // 插件版本号
+  //
+  with AIPlugin.RegFunction(FUN_BLLFORMSHOW, '登录') do
+    ShowEntrance := False;  // 在界面显示调用入口
+end;
+
+procedure ExecFunction(const AIFun: ICustomFunction); stdcall;
+var
+  vID: string;
+  vIFun: IFunBLLFormShow;
+begin
+  vID := AIFun.ID;
+  if vID = FUN_BLLFORMSHOW then  // 显示业务窗体
+  begin
+    vIFun := TFunBLLFormShow.Create;
+    vIFun.AppHandle := (AIFun as IFunBLLFormShow).AppHandle;
+    //Application.Handle := vIFun.AppHandle;
+    vIFun.ShowEntrance := (AIFun as IFunBLLFormShow).ShowEntrance;  // 显示入口点
+    vIFun.OnNotifyEvent := (AIFun as IFunBLLFormShow).OnNotifyEvent;  // 插件事件
+
+    PluginShowLoginForm(vIFun);
+  end
+  else
+  if vID = FUN_BLLFORMDESTROY then  // 业务窗体关闭
+    PluginCloseLoginForm;
+end;
+
+procedure UnLoadPlugin(const AIPlugin: IPlugin); stdcall;
+begin
+  PluginCloseLoginForm;
+end;
+
+end.
---
+++ ClientPlugin/Login/frm_login.pas
@@ -0,0 +1,170 @@
+unit frm_login;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Vcl.Graphics, Vcl.Controls,
+  Vcl.Forms, FunctionIntf, Vcl.StdCtrls, Vcl.Dialogs;
+
+type
+  TfrmLogin = class(TForm)
+    btnOk: TButton;
+    lbl1: TLabel;
+    edtUserID: TEdit;
+    edtPassword: TEdit;
+    btnCancel: TButton;
+    lbl2: TLabel;
+    lblSet: TLabel;
+    procedure FormCreate(Sender: TObject);
+    procedure btnOkClick(Sender: TObject);
+    procedure btnCancelClick(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure lblSetClick(Sender: TObject);
+  private
+    { Private declarations }
+    FUserID: string;
+    FOnFunctionNotify: TFunctionNotifyEvent;
+  public
+    { Public declarations }
+  end;
+
+  procedure PluginShowLoginForm(AIFun: IFunBLLFormShow);
+  procedure PluginCloseLoginForm;
+
+var
+  frmLogin: TfrmLogin;
+  PlugID: string;
+
+implementation
+
+uses
+  PluginConst, FunctionConst, FunctionImp, emr_Common, emr_BLLServerProxy,
+  emr_MsgPack, emr_BLLConst, emr_PluginObject, emr_Entry, IdHashMessageDigest,
+  FireDAC.Comp.Client, frm_ConnSet;
+
+{$R *.dfm}
+
+procedure PluginShowLoginForm(AIFun: IFunBLLFormShow);
+var
+  vUserInfo: IUserInfo;
+begin
+  if FrmLogin = nil then
+    FrmLogin := Tfrmlogin.Create(nil);
+
+  FrmLogin.FOnFunctionNotify := AIFun.OnNotifyEvent;
+
+  FrmLogin.ShowModal;
+
+  if FrmLogin.ModalResult = mrOk then
+  begin
+    vUserInfo := TUserInfoIntf.Create;
+    vUserInfo.UserID := FrmLogin.FUserID;
+    FrmLogin.FOnFunctionNotify(PlugID, FUN_USERINFO, vUserInfo);  // 告诉主程序登录用户名
+  end;
+
+  FrmLogin.FOnFunctionNotify(PlugID, FUN_BLLFORMDESTROY, nil);  // 释放业务窗体资源
+end;
+
+procedure PluginCloseLoginForm;
+begin
+  if FrmLogin <> nil then
+    FreeAndNil(FrmLogin);
+end;
+
+procedure TfrmLogin.btnCancelClick(Sender: TObject);
+begin
+  Close;
+end;
+
+procedure TfrmLogin.btnOkClick(Sender: TObject);
+begin
+  BLLServerExec(
+    procedure(const ABLLServerReady: TBLLServerProxy)  // 获取登录用户的信息
+    var
+      vExecParam: TMsgPack;
+      vPAWMD5: TIdHashMessageDigest5;
+    begin
+      ABLLServerReady.Cmd := BLL_LOGIN;  // 核对登录信息
+      vExecParam := ABLLServerReady.ExecParam;
+      vExecParam.S[TUser.ID] := edtUserID.Text;
+      vPAWMD5 :=  TIdHashMessageDigest5.Create;
+      try
+        vExecParam.S[TUser.Password] := vPAWMD5.HashStringAsHex(edtPassword.Text);
+      finally
+        vPAWMD5.Free;
+      end;
+
+      ABLLServerReady.AddBackField(TUser.ID);
+    end,
+    procedure(const ABLLServer: TBLLServerProxy; const AMemTable: TFDMemTable = nil)
+    begin
+      if not ABLLServer.MethodRunOk then  // 服务端方法返回执行不成功
+      begin
+        ShowMessage(ABLLServer.MethodError);
+        Exit;
+      end;
+      if ABLLServer.RecordCount = 1 then
+      begin
+        FUserID := ABLLServer.BackField(TUser.ID).AsString;
+        Self.ModalResult := mrOk;
+      end
+      else
+      if ABLLServer.RecordCount = 0 then
+        ShowMessage('登录失败：无效的用户或者错误的密码！')
+      else
+      if ABLLServer.RecordCount > 1 then
+        ShowMessage('登录失败：存在多个相同的用户，请管理员确认');
+    end);
+end;
+
+procedure TfrmLogin.FormCreate(Sender: TObject);
+begin
+  PlugID := PLUGIN_LOGIN;
+  GClientParam := TClientParam.Create;
+  //SetWindowLong(Handle, GWL_EXSTYLE, (GetWindowLong(handle, GWL_EXSTYLE) or WS_EX_APPWINDOW));
+end;
+
+procedure TfrmLogin.FormDestroy(Sender: TObject);
+begin
+  GClientParam.Free;
+end;
+
+procedure TfrmLogin.FormShow(Sender: TObject);
+var
+  vServerInfo: IServerInfo;
+begin
+  vServerInfo := TServerInfo.Create;
+
+  FOnFunctionNotify(PlugID, FUN_BLLSERVERINFO, vServerInfo);
+  GClientParam.BLLServerIP := vServerInfo.Host;
+  GClientParam.BLLServerPort := vServerInfo.Port;
+
+  FOnFunctionNotify(PlugID, FUN_MSGSERVERINFO, vServerInfo);
+  GClientParam.MsgServerIP := vServerInfo.Host;
+  GClientParam.MsgServerPort := vServerInfo.Port;
+
+  FOnFunctionNotify(PlugID, FUN_UPDATESERVERINFO, vServerInfo);
+  GClientParam.UpdateServerIP := vServerInfo.Host;
+  GClientParam.UpdateServerPort := vServerInfo.Port;
+end;
+
+procedure TfrmLogin.lblSetClick(Sender: TObject);
+var
+  vFrmConnSet: TfrmConnSet;
+begin
+  vFrmConnSet := TfrmConnSet.Create(Self);
+  try
+    vFrmConnSet.edtBLLServerIP.Text := GClientParam.BLLServerIP;
+    vFrmConnSet.edtBLLServerPort.Text := GClientParam.BLLServerPort.ToString;
+    vFrmConnSet.edtMsgServerIP.Text := GClientParam.MsgServerIP;
+    vFrmConnSet.edtMsgServerPort.Text := GClientParam.MsgServerPort.ToString;
+    vFrmConnSet.edtUpdateServerIP.Text := GClientParam.UpdateServerIP;
+    vFrmConnSet.edtUpdateServerPort.Text := GClientParam.UpdateServerPort.ToString;
+    vFrmConnSet.ShowModal;
+  finally
+    FreeAndNil(vFrmConnSet);
+  end;
+end;
+
+end.
